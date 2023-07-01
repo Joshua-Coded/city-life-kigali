aa92de5316c3473f7200847849c982e92402cce8
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-01 10:55:37 +0200

    logo added

---
+++ city-life/app/components/Container.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+
+const Container: React.FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+        <div className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-20
+        md:px-10
+        sm:pz-2
+        px-4
+        ">
+            {children}
+        </div>
+    )
+}
+
+export default Container; 
--- city-life/app/page.tsx
+++ city-life/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="text-cyan-500 text-2xl">City-Life </div>
+    <div className="text-cyan-500 text-2xl">
+      
+    </div>
     )
 }
