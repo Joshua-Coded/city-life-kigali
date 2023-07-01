a9066bbbc31a4506cde1e4f3ec4f12a5f57e18a1
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-01 12:56:41 +0200

    user menu

---
+++ city-life/app/components/Avatar.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Image from 'next/image';
+const Avatar = () => {
+    return (
+        <Image
+        alt="avatar"
+        className="rounded-full"
+        height="30"
+        width="30"
+        src="/images/placeholder.jpg"
+        
+        />
+    )
+}
+export default Avatar;
--- city-life/app/components/navbar/UserMenu.tsx
+++ city-life/app/components/navbar/UserMenu.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-
+import {AiOutlineMenu} from 'react-icons/ai';
+import Avatar from '../Avatar';
 const UserMenu = () => {
     return (
         <div className="relative">
             <div className="
-            flex
-             flex-row
-              items-center
-              gap-3">
+            flex flex-row items-center gap-3">
                 <div 
                 onClick={() => {}}
                 className="
@@ -20,11 +18,38 @@ const UserMenu = () => {
                 py-3
                 px-4
                 rounded-full
-                hover:bg-neutral-200
+                hover:bg-neutral-100
                 transition
                 cursor-pointer"
                 >
-City Life Your Home
+                Kigali My Home
+                </div>
+                <div 
+                onClick={() => {}}
+                className="
+                p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+                "
+                >
+                    <AiOutlineMenu />
+                        <div className="
+                        hidden md:block
+                        ">
+                            
+                            <Avatar />
+                        </div>
+
                 </div>
               </div>
         </div>
