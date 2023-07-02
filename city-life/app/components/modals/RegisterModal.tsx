0e1ed99e32a798195ba3cbc96e1681e6b754ca61
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-02 12:37:33 +0200

    register modal

--- city-life/app/components/modals/RegisterModal.tsx
+++ city-life/app/components/modals/RegisterModal.tsx
@@ -11,10 +11,50 @@ import {
 } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from './Modal';
 
 const RegisterModal = () => {
+    const registerModal = useRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/register', data)
+        .then(() => {
+            registerModal.onClose();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        })
+    }
+    
     return (
-<div></div>
+            <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title='Register'
+            actionLabel='Continue'
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}            
+            />
     )    
 }
 
