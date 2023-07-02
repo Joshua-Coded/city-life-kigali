160e1a0d25d8c6df8e71c5a9b95fe01dd103eced
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-02 13:24:12 +0200

    register modal done

--- city-life/app/components/modals/RegisterModal.tsx
+++ city-life/app/components/modals/RegisterModal.tsx
@@ -63,7 +63,27 @@ const RegisterModal = () => {
                 register={register}
                 errors={errors}
                 required
-/>
+                />
+
+                <Input
+                id='name'
+                label='Name'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                />
+                
+                <Input
+                id='password'
+                type='password'
+                label='Password'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                />
+                
         </div>
     )
     
